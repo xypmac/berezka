f3ddbc50cef2ba07a89bb640ffdeec486a3d17ed
Алина Четкарёва <mosenka91@mail.ru>
2021-08-02 13:30:18 +0400

    fixed bug summer-camp/accommodation page (scroll)

--- local/templates/main/components/bitrix/news.list/accommodation/script.js
+++ local/templates/main/components/bitrix/news.list/accommodation/script.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
     $('.accommodation__item').click(function(){
         $('.accommodation__preview').children().remove();
         $(this).children('.accommodation__img').clone().appendTo($('.accommodation__preview'));
+        $('html, body').animate({
+            scrollTop: $('.accommodation__preview').offset().top  
+        }, 300);
     });
+    
 });
