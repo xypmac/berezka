09e2bc73f63b2d9f1b39fd92c6dc143dc7d11831
Алина Четкарёва <mosenka91@mail.ru>
2021-08-11 11:24:45 +0400

    add entertainment section for recreation-center page

--- include/entertainment.php
+++ include/entertainment.php
@@ -1,1 +1,34 @@
-тут будет описание для двух разделов <a href="/recreation-center/event">тут</a> и <a href="/recreation-center/additional-services">Тут</a><br>
+<!-- тут будет описание для двух разделов <a href="/recreation-center/event">тут</a> и <a href="/recreation-center/additional-services">Тут</a><br> -->
+<section class="parts parts-recreation">
+    <div class="container">
+        <div class="row">
+            <h2 class="parts__title">Развлечения и досуг</h2>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-11  parts__wrapper parts__wrapper-recreation">
+                <div class="parts__item part part-services">
+                    <div class="part__title">
+                        <h3 class="part__text">Дополнительные услуги</h3>
+                        <a href="/" class="arrows-container">
+                            <span class="arrow arrow-one"></span>
+                            <span class="arrow arrow-two"></span>
+                            <span class="arrow arrow-three"></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="parts__item part part-event">
+                    <div class="part__title">
+                        <h3 class="part__text">Ивент мероприятия</h3>
+                        <a href="/" class="arrows-container">
+                            <span class="arrow arrow-one"></span>
+                            <span class="arrow arrow-two"></span>
+                            <span class="arrow arrow-three"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
