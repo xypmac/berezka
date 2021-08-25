958f894e4dd8d25698a1d31b3a45b59f79693539
Алина Четкарёва <mosenka91@mail.ru>
2021-08-25 11:57:29 +0400

    add banquets-and-buffets page

---
+++ include/banquets-and-buffets.php
@@ -0,0 +1,135 @@
+<section class="section-notes">
+    <div class="container">
+        <div class="row">
+            <h2 class="notes__title">Банкеты и фуршеты</h2>
+        </div>
+    </div>
+    <article class="article-notes lines-two">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 notes__wrapper notes__wrapper-right">
+                    <div class="notes__item notes__item-img notes__item-img-right">
+                        <img class="notes__img notes__img-left" src="<?=SITE_TEMPLATE_PATH?>/img/bg_banquet.jpg" alt="">
+                    </div>
+                    <div class="notes__item notes__item-text note note-green">
+                        <div class="note__title">
+                            Банкет
+                        </div>
+                        <div class="note__text">
+                            <p>На нашей базе отдыха вы можете организовать любое мероприятие, а накормить гостей можно как в формате банкета, так и в формате фуршета. Несколько банкетных залов от 40 до 250 человек, живописная и ухоженная прилегающая территория, пожалуй идеально подойдут для корпоратива, свадьбы или юбилея.</p>
+                        </div>
+                        <div class="note__card note__card-right note__card-orange">
+                            <div class="note__card-text">
+                                Полный ассортимент блюд (первое, второе, десерты, горячие и холодные закуски) - наши профессиональные повара подготовят меню к вашему мероприятию. Алкоголь и десерты - допускаются свои.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
+<section class="section-banquet banquet">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="banquet__wrapper">
+                <div class="banquet__title">
+                    Меню банкета
+                </div>
+                <div class="banquet__text">
+                    <div class="banquet__icon"></div>
+                    <a href="/" class="banquet__link">Посмотреть</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 banquet__description">
+                К вашим услугам любое банкетное обслуживание от фуршета до грандиозного застолья. Вместимость залов от 40 до 250 человек.
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section">
+    <article class="article-notes lines-one">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 notes__wrapper notes__wrapper-first notes__wrapper-left">
+                    <div class="notes__item notes__item-img notes__item-img-left">
+                        <img class="notes__img notes__img-left" src="<?=SITE_TEMPLATE_PATH?>/img/buffets.jpg" alt="">
+                    </div>
+                    <div class="notes__item notes__item-text note note-orange">
+                        <div class="note__title">
+                            Фуршет
+                        </div>
+                        <div class="note__text">
+                            <p>Фуршет очень уместен на конференциях, семинарах, фестивалях, при проведении деловых переговоров, свадеб, командообразующих мероприятий, а также для корпоративных вечеринок с танцами и любых неформальных мероприятий.</p>
+                            <p>На протяжении всего вечера или дня стол остается накрытым и выглядит элегантно.</p>
+                       </div>
+                        <div class="note__card note__card-left  note__card-pink">
+                            <div class="note__card-text">
+                                Угощений может быть множество - это и закуски, и горячее, и десерты. Фуршет располагает гостей к общению и не дает заскучать на одном месте.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
+<section class="section-banquet banquet">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="banquet__wrapper">
+                <div class="banquet__title">
+                    Меню фуршета
+                </div>
+                <div class="banquet__text">
+                    <div class="banquet__icon"></div>
+                    <a href="/" class="banquet__link">Посмотреть</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 banquet__description">
+                Фуршетное меню состоит из ассорти (мясных, рыбных, овощных или фруктовых), канапе, шашлычков, салатов в тарталетках, рулетов напитков и большого ассортимента выпечки и десертов. 
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-place">
+    <div class="container">
+        <div class="row">
+            <h2 class="place__title">
+                Места проведения
+            </h2>
+            <div class="place__wrapper">
+                <div class="place__item">
+                    <div class="place__img-wrapper">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/bg_banquet.jpg" alt="" class="place__img">
+                    </div>
+                    <div class="place__card place__card-blue">
+                        <div class="place__card-text">Столовая</div>
+                    </div>
+                    <div class="place__text">
+                        На базе отдыха работает столовая. Мы предлагаем гибкий подход к организации питания. Это и традиционные каши, омлеты и блинчики на завтрак, солянка, уха, борщ, окрошка, большой выбор вторых блюд на обед, вкусные легкие ужины и, конечно, салаты, компоты, выпечка, фрукты. Можно заказать комплексные завтраки, обеды и ужины или отдельные блюда. Изумительная выпечка: пирожки, сладкие булочки с повидлом, изюмом, расстегаи, этот список можно продолжать бесконечно. Столовая рассчитана на размещение 250 человек, здесь проводят юбилеи, свадьбы, корпоративные вечера. На первом этаже столовой организована многофункциональная площадка. 
+                    </div>
+                </div>
+                <div class="place__item">
+                    <div class="place__img-wrapper">
+                        <img src="<?=SITE_TEMPLATE_PATH?> /img/place.png" alt="" class="place__img">
+                    </div>
+                    <div class="place__card place__card-pink">
+                        <div class="place__card-text">Кошкин дом</div>
+                    </div>
+                    <div class="place__text">
+                        На базе отдыха работает столовая. Мы предлагаем гибкий подход к организации питания. Это и традиционные каши, омлеты и блинчики на завтрак, солянка, уха, борщ, окрошка, большой выбор вторых блюд на обед, вкусные легкие ужины и, конечно, салаты, компоты, выпечка, фрукты. Можно заказать комплексные завтраки, обеды и ужины или отдельные блюда. Изумительная выпечка: пирожки, сладкие булочки с повидлом, изюмом, расстегаи, этот список можно продолжать бесконечно. Столовая рассчитана на размещение 250 человек, здесь проводят юбилеи, свадьбы, корпоративные вечера. На первом этаже столовой организована многофункциональная площадка. 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
