6f564bfd07b1da9bbbc7938bc20414c6c088e147
valeraizh1992 <xypmac@yandex.ru>
2021-08-27 10:00:09 +0400

    fix

--- local/templates/main/components/bitrix/catalog.section.list/menu_food/script.js
+++ local/templates/main/components/bitrix/catalog.section.list/menu_food/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
 
 
     let setting = {
@@ -12,23 +12,22 @@ $(document).ready(function(){
     $(document).on('click', '.food', function(){
         $('.header').css({'z-index': '-1'});
         $('.foods__wrapper').css({'z-index': '-1'});
-        $('.meal__fade:first').fadeIn();
+        $(this).closest('.section-foods').find('.meal__fade').fadeIn();
         $('body').css({'overflow': 'hidden'});
         return false;
-      });
-      
-  
-  
+    });
+
+
     // Клик по ссылке "Закрыть".
-    $(document).on('click', '.meal__close', function(){ 
+    $(document).on('click', '.meal__close', function(){
         $('.header').css({'z-index': '1'});
         $('.foods__wrapper').css({'z-index': '1'});
         $(this).parents('.meal__fade').fadeOut();
         $('body').css({'overflow': 'auto'});
         return false;
     });
-   
-    
+
+
     // Закрытие по клавише Esc.
     $(document).keydown(function(e) {
         if (e.keyCode === 27) {
@@ -39,34 +38,34 @@ $(document).ready(function(){
             $('body').css({'overflow': 'auto'});
         }
     });
-    
+
     // Клик по фону, но не по окну.
-    $(document).on('click', '.meal__fade', function(e){ 
+    $(document).on('click', '.meal__fade', function(e){
         if ($(e.target).closest('.meal__popup').length == 0) {
             $(this).fadeOut();
             $('.header').css({'z-index': '1'});
             $('.foods__wrapper').css({'z-index': '1'});
-            $('body').css({'overflow': 'auto'});					
+            $('body').css({'overflow': 'auto'});
         }
 
     });
-  
-  
+
+
     function windowSize(){
         if ($(window).width() < '575.98'){
             $('.foods__wrapper:not(.slick-initialized)').slick(setting);
             if($(".foods__wrapper").hasClass("slick-initialized")) {
                 $(".foods__wrapper")[0].slick.refresh();
-            } 
-            
+            }
+
         } else {
             if($(".foods__wrapper").hasClass("slick-initialized")) {
                 $(".foods__wrapper").slick('unslick');
             }
         }
     }
-  
-    $(window).on('load resize',windowSize); 
+
+    $(window).on('load resize',windowSize);
 
 
 });
